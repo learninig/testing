5dd9b53a276a4203929e71e84208553c06ef3931
Manoj <reddynaveenp1@gmail.com>
2014-07-28 17:36:56 +0530

    delete some spaces

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -37,14 +37,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
-		
-		
-		
-		
-		
-		service.service();
-		
+		System.out.println("Delete some spaces");
 		return "home";
 	}
 	
