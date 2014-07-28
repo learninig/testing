23793ac0b575ddf617a728af14a9757dff7d270d
NagendraReddyAnchala <anchalanagendrareddy@gmail.com>
2014-07-28 17:38:30 +0530

    added repository package

---
+++ Sample/src/main/java/com/sample/repository/HomeRepository.java
@@ -0,0 +1,14 @@
+package com.sample.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomeRepository {
+
+	public void service() {
+		System.out.println("this is repository service method....");
+		
+	}
+
+	
+}
