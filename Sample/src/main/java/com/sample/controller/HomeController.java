4e0bc4e05003fa9a78072403d5c769088e45a020
NagendraReddyAnchala <anchalanagendrareddy@gmail.com>
2014-07-28 17:16:38 +0530

    added a package and service class

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -6,16 +6,21 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sample.service.HomeService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	@Autowired
+	HomeService service;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -32,6 +37,13 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		
+		
+		
+		
+		
+		service.service();
 		
 		return "home";
 	}
---
+++ Sample/src/main/java/com/sample/service/HomeService.java
@@ -0,0 +1,11 @@
+package com.sample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+	public void service(){
+		System.out.println("this is homeServive");
+	}
+
+}
