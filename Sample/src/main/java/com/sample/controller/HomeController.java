26675253545b483a12bafe2b48622da204b6e1ad
srinivas <allu12@gmail.com>
2014-07-28 17:22:43 +0530

    sample

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.sample.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -16,20 +14,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		logger.info("Added new statement");
+		// Date date = new Date();
+		// DateFormat dateFormat =
+		// DateFormat.getDateTimeInstance(DateFormat.LONG,
+		// DateFormat.LONG, locale);
+		//
+		// String formattedDate = dateFormat.format(date);
+		//
+		// model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
 }
