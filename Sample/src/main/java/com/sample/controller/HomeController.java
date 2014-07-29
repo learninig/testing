408ebf5d5eb4ebf3df52d89014ab62fa53139d66
sravani <sravani.nagalla@lsnsoft.in>
2014-07-29 10:24:09 +0530

    added service package and injected service in controller

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -2,21 +2,23 @@ package com.sample.controller;
 
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.sample.domain.Message;
+import com.sample.service.HomeService;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+
 	@Autowired
-	HomeDao dao;
+	HomeService homeService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -24,12 +26,12 @@ public class HomeController {
 		Message message = new Message();
 		message.setId("12345");
 		message.setMessage("this is message related to 12345");
-		dao.print(message);
+		homeService.print(message);
 		return "home";
 	}
-public void calculate()
-	{
-	System.out.println("Here some calculation");
-	System.out.println("Here employee salary is calculated");
+
+	public void calculate() {
+		System.out.println("Here some calculation");
+		System.out.println("Here employee salary is calculated");
 	}
 }
--- Sample/src/main/java/com/sample/dao/HomeDao.java
+++ Sample/src/main/java/com/sample/dao/HomeDao.java
@@ -1,12 +1,13 @@
 package com.sample.dao;
 
-public class HomeDao {
+import com.sample.domain.Message;
 
+public class HomeDao {
 
 	public void print(Message message) {
 		System.out.println(message);
 		System.out.println("now we r in print method of dao  class");
-		
+
 	}
 
 }
