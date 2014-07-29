4bec476a0809e239e30632fe0bc61869a051071b
naidu <naidu@gmail.com>
2014-07-29 10:43:34 +0530

    check arrows

--- Sample/src/main/java/com/sample/dao/HomeDao.java
+++ Sample/src/main/java/com/sample/dao/HomeDao.java
@@ -3,11 +3,16 @@ package com.sample.dao;
 import com.sample.domain.Message;
 
 public class HomeDao {
+	public void service() {
+		// TODO Auto-generated method stub
+		System.out.println("this is my service");
+	}
+
 
 	public void print(Message message) {
 		System.out.println(message);
 		System.out.println("now we r in print method of dao  class");
-
+		
 	}
 
 }
--- Sample/src/main/java/com/sample/service/HomeService.java
+++ Sample/src/main/java/com/sample/service/HomeService.java
@@ -7,3 +7,25 @@ public interface HomeService {
 	void print(Message message);
 
 }
+package com.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.sample.dao.HomeDao;
+
+@Service
+public class HomeService {
+	@Autowired
+	private HomeDao homeDao;
+	public void service(){
+
+		homeDao.service();
+		System.out.println("1 in service");
+
+
+
+	}
+
+}
